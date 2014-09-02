a7c45947f614dc2be17f0817ac0e2a1005a4dd74
Erik Schwartz <erik@erikschwartz.net>
2014-09-01 20:07:30 -0700

    Set CE Closed column to boolean

--- createCodeEnforcementCKAN.sh
+++ createCodeEnforcementCKAN.sh
@@ -12,7 +12,7 @@ curl -v $CKAN_INSTANCE/resource_create \
 
 sleep 5
 
-fields='[{"id":"CaseNo","type":"text"},{"id":"CaseYr","type":"text"},{"id":"Address","type":"text"},{"id":"DateOpened","type":"date"},{"id":"CaseType","type":"text"},{"id":"Status","type":"text"},{"id":"StatusDate","type":"date"},{"id":"Closed","type":"text"},{"id":"parcelId","type":"text"},{"id":"lat","type":"float"},{"id":"lng","type":"float"}]'
+fields='[{"id":"CaseNo","type":"text"},{"id":"CaseYr","type":"text"},{"id":"Address","type":"text"},{"id":"DateOpened","type":"date"},{"id":"CaseType","type":"text"},{"id":"Status","type":"text"},{"id":"StatusDate","type":"date"},{"id":"Closed","type":"bool"},{"id":"parcelId","type":"text"},{"id":"lat","type":"float"},{"id":"lng","type":"float"}]'
 
 curl -v $CKAN_INSTANCE/datastore_create \
   -d '{"resource_id": "'$RESOURCE_ID'", "fields": '$fields', "primary_key":"CaseNo", "force":"true"}' \
