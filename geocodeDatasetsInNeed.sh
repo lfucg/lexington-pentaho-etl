64595454abc5fa51977714b307e0cf616d0094e0
Erik Schwartz <erik@erikschwartz.net>
2014-10-04 17:20:57 -0400

    No need to source .env file. Using kettle.properties

--- geocodeDatasetsInNeed.sh
+++ geocodeDatasetsInNeed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # meant to be run from a cronjob on server
-cd ~/pentaho-transformations && source ~/pentaho-transformations/.env.pentaho && ~/data-integration/pan.sh -level=Basic -file "geocode-code-enforcement.ktr" >> logs/cron.log 2>&1
-cd ~/pentaho-transformations && source ~/pentaho-transformations/.env.pentaho && ~/data-integration/pan.sh -level=Basic -file "geocode-building-permits.ktr" >> logs/cron.log 2>&1
+cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "geocode-code-enforcement.ktr" >> logs/cron.log 2>&1
+cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "geocode-building-permits.ktr" >> logs/cron.log 2>&1
