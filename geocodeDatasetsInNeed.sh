07229d076d066f0fa769359b7a7ddba07d450d18
Erik Schwartz <erik@erikschwartz.net>
2014-10-17 15:55:17 -0700

    Add foreclosure sales to cron scripts

--- geocodeDatasetsInNeed.sh
+++ geocodeDatasetsInNeed.sh
@@ -3,3 +3,4 @@
 # meant to be run from a cronjob on server
 cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "geocode-code-enforcement.ktr" >> logs/cron.log 2>&1
 cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "geocode-building-permits.ktr" >> logs/cron.log 2>&1
+cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "parcel-lookup-foreclosure-sales.ktr" >> logs/cron.log 2>&1
--- upsertDatasets.sh
+++ upsertDatasets.sh
@@ -3,3 +3,4 @@
 # meant to be run from a cronjob on server
 cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "pull-last-day-of-code-enforcement.ktr" >> logs/cron.log 2>&1
 cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "pull-last-day-of-building-permits.ktr" >> logs/cron.log 2>&1
+cd ~/pentaho-transformations && ~/data-integration/pan.sh -level=Basic -file "pull-last-day-of-foreclosure-sales" >> logs/cron.log 2>&1
