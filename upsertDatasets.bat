190a788272eb9ebb5a62ab574979ae69bc047954
sueb <sueb@lexingtonky.gov>
2014-10-31 11:38:19 -0400

    Pull latest changes before running

--- upsertDatasets.bat
+++ upsertDatasets.bat
@@ -1,2 +1,4 @@
 cd C:\projects\lexington-pentaho-etl\
+call git pull origin master
+:: set KETTLE_HOME="C:\projects\lexington-pentaho-etl\.kettle\"
 call data-integration\Pan.bat -level=Basic -file "pull-last-day-of-foreclosure-sales.ktr" 
