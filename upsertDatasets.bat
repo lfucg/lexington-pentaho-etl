295eee7716c044ba26a923590fa8d216399fa22c
Erik Schwartz <erik@erikschwartz.net>
2014-12-15 20:05:23 -0800

    Call parcel lookup after pulling foreclosures

--- upsertDatasets.bat
+++ upsertDatasets.bat
@@ -1,4 +1,6 @@
 cd C:\projects\lexington-pentaho-etl\
 call git pull origin master
-:: set KETTLE_HOME="C:\projects\lexington-pentaho-etl\.kettle\"
-call data-integration\Pan.bat -level=Basic -file "pull-last-day-of-foreclosure-sales.ktr" 
+
+cd data-integration
+call Pan.bat -level=Basic -file "../pull-last-day-of-foreclosure-sales.ktr" 
+call Pan.bat -level=Basic -file "../parcel-lookup-foreclosure-sales.ktr" 
